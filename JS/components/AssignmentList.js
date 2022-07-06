2278564a5a1b1980f18c5fa4be43a1ef8767e3e9
LODOWNTHROWDOWN <martinlauren06@gmail.com>
2022-07-06 14:51:40 -0400

    Episode 10
    Just played around with the functionality mostly and learned how to create custom arrays

--- JS/components/AssignmentList.js
+++ JS/components/AssignmentList.js
@@ -6,11 +6,25 @@ export default {
   template: `
       <section v-show="assignments.length">
 
-        <h2 class="font-bold mb-2">{{ title }}</h2>
+        <h2 class="font-bold mb-2">
+        {{ title }}
+        <span>({{ assignments.length }})</span>
+        </h2>
 
-        <ul class="border border-gray-600 divide-y divide-gray-600">
+        <div class="flex gap-2">
+          <button
+                  @click="currentTag = tag"
+                  v-for="tag in tags"
+                  class="border rounded px-1 py-px text-xs"
+                  :class="{
+                    'border-blue-500 text-blue': tag == currentTag
+                  }"
+          >{{ tag }}</button>
+        </div>
+
+        <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
           <assignment
-              v-for="assignment in assignments"
+              v-for="assignment in filteredAssignments"
               :key="assignment.id"
               :assignment="assignments"
           ></assignment>
@@ -22,5 +36,23 @@ export default {
       props: {
         assignments: Array,
         title: String
+      },
+
+      data() {
+        return {
+          currentTag: ''
+        };
+      }
+
+      computed: {
+        filteredAssignments() {
+          if (this.currentTag === 'all') {
+            return this.assignments;
+          }
+          return this.assignments.filter(a => a.tag === this.currentTag);
+        }
+        tags () {
+          return ['all', ...new Set(this.assignments.map(a => a.tag))];
+        }
       }
 }
