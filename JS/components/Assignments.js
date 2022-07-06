885a9d96ba4c68b9b104b3ffebff72804177495a
LODOWNTHROWDOWN <martinlauren06@gmail.com>
2022-07-06 14:14:10 -0400

    Episode 9
    Learned about how the parent communicates with the child and vice versa
    
    using props for the parent to child communication and emit for child to parent communication

--- JS/components/Assignments.js
+++ JS/components/Assignments.js
@@ -1,7 +1,8 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 
 export default {
-  components: { AssignmentList },
+  components: { AssignmentList, AssignmentCreate },
 
   template: `
   <section class="space-y-6">
@@ -9,12 +10,7 @@ export default {
 
       <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
 
-      <form @submit.prevent="add">
-        <div class="border border-gray-600 text-black">
-          <input placeholder="New assignment..." class="p-2" />
-          <button type="submit" class="bg-white p-2 border-l">Add</button>
-        </div>
-      </form>
+      <assignment-create @add="add"></assignment-create>
   </section>
   `,
 
@@ -40,15 +36,12 @@ export default {
     },
 
     methods: {
-      add() {
+      add(name) {
         this.assignments.push({
-          name: this.newAssignment,
+          name: name,
           completed: false,
           id: this.assignments.length + 1
         });
-
-        this.newAssignment = '';
-      }
     }
   }
 }
